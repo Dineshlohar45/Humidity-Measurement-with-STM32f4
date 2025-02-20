9f40ef33a46c8ea394f821b70cf1ff78102271d8
Dinesh Gadilohar <lohardinesh045@gmail.com>
2025-02-21 02:15:25 +0530

    Create hum_sensor.c

---
+++ hum_sensor.c
@@ -0,0 +1,9 @@
+#include "humidity_sensor.h"
+#include "main.h"
+
+// Dummy function to simulate reading from a humidity sensor
+float read_humidity(void)
+{
+  // Simulate humidity reading
+  return 45.0f;
+}
